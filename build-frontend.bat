6610bbc81ba243ec818ea36d4fc905bbc0d7f50e
Zekken <brodycas3@gmail.com>
2026-03-25 12:48:10 -0400

    Update build-frontend.bat
    Smaller

--- build-frontend.bat
+++ build-frontend.bat
@@ -1,15 +1,9 @@
 @echo off
-:: Change directory to the script's location, then into 'frontend'
 cd /d "%~dp0frontend"
 
-echo Installing dependencies...
 call npm install
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-echo Building frontend...
-call npm run build
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-echo Frontend built successfully to static/
+if %errorlevel% equ 0 (
+    call npm run build
+)
 
 pause
